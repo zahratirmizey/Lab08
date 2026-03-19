3a07d593c0f8c8944be93aa66d413624d2b7cb17
zahratirmizey <26100147@lums.edu.pk>
2026-03-19 15:45:43 +0500

    Testing DeleteCity (pass)

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,7 +18,10 @@ public class CustomList {
         return cities.contains(city);
     }
 
-
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public void deleteCity(City city) {
+        if (!cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.remove(city);
+    }
 }
