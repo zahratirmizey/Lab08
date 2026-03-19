011def3596026619b64f211d1e95f916a9b4a206
zahratirmizey <26100147@lums.edu.pk>
2026-03-19 15:48:30 +0500

    Testing CountCities (fail)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -20,4 +20,17 @@ public class CustomListTest {
         assertFalse(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(1, list.countCities());
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        assertEquals(2, list.countCities());
+        list.deleteCity(calgary);
+        assertEquals(1, list.countCities());
+    }
+
 }
